f54de436aa0cab23d900f6ea45fd9df67ca4947a
Marcel Schäfer <mar1006.schaefer@gmail.com>
2026-04-28 09:15:48 +0200

    Zahlen @app.get erstellt

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/name/{name}")
+def greet_name(name:str):
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/rechne/{zahl}")
+def rechne (zahl: float):
+result = (zahl +2)*3
+return {"message": f"Das Ergebnis von {zahl} ist {ergebnis}"}
+#zwei weiter @app.get "dinger bauen wo man einen wert rein bekommt und diesen Weiterverarbeite (z.B Uhrzeit.... und die nächste volle stunde ist wenn 12.40 eingegeben wird dass dann 13 Uhr raus kommt. Und dann z.B Zahl die eingegeben wird plus 2 und dann mal 3)"
